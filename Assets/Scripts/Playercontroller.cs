9fa1e6ce4443b1f6b93cd5b98dafb2879a70f69c
Fulgore2002 <115446255+Fulgore2002@users.noreply.github.com>
2025-09-19 15:15:54 -0500

    Made a basic Controller script

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
 
-public class Playercontroller
+public class Playercontroller : MonoBehaviour
 {
-    
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Move left/right using joystick
+        float move = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y);
+
+        // Jump when "A" button is pressed
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.linearVelocity.y) < 0.01f)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
 }
