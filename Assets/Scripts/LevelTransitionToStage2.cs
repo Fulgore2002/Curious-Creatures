4cd6b7e565c0fc37be2fda74a63947a07dc30969
Trent <123410996+Glacoi@users.noreply.github.com>
2025-12-11 13:31:47 -0600

    LevelTransitiontest

--- Assets/Scripts/LevelTransitionToStage2.cs
+++ Assets/Scripts/LevelTransitionToStage2.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class LevelTransitionToStage2 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool hasTransitioned = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (hasTransitioned) return;
+
+        if (other.CompareTag("Player"))
+        {
+            hasTransitioned = true;
+
+            // No disabling player movement here!
+
+            StartCoroutine(LoadNextStage());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator LoadNextStage()
     {
-        
+        yield return new WaitForSeconds(0.5f); // small delay
+        SceneManager.LoadScene("Karsiori_Stage2");
     }
 }
